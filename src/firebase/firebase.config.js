1a1bdf062e94e417f04ca16caab9fb500142be6c
husneara <husnearaa99@gmail.com>
2023-10-18 22:11:14 +0600

    has completed the firebase setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNazBdTAy68uJk0WGfdnpnPgMUtTyc1vE",
+  authDomain: "beauty-world-a21cb.firebaseapp.com",
+  projectId: "beauty-world-a21cb",
+  storageBucket: "beauty-world-a21cb.appspot.com",
+  messagingSenderId: "224979908590",
+  appId: "1:224979908590:web:ee9407f00abd522049c0ff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
